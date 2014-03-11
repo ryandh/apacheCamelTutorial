98c669513510e774676ab575dea9d9733c7f7059
ryandh <androidyou2010@gmail.com>
2014-03-10 18:28:55 -0700

    demoxml

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,17 +1,10 @@
+import java.io.InputStream;
 import java.util.Date;
-import java.util.Map;
-import java.util.Set;
-
-import javax.activation.DataHandler;
-
-import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
-import org.apache.camel.InvalidPayloadException;
-import org.apache.camel.Message;
 import org.apache.camel.Processor;
-import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.RoutesDefinition;
 
 public class Program {
 
@@ -21,30 +14,15 @@ public class Program {
 	 */
 	public static void main(String[] args) throws Exception {
 
-		CamelContext context = new DefaultCamelContext();
-		RoutesBuilder builder = new DummyRoutesBuild();
-		// Add route definication
-		context.addRoutes(builder);
+		DefaultCamelContext context = new DefaultCamelContext();
+
+		InputStream inputStream = Program.class
+				.getResourceAsStream("route.xml");
+		RoutesDefinition rd = context.loadRoutesDefinition(inputStream);
+		context.addRouteDefinitions(rd.getRoutes());
+
 		context.start();
 		System.in.read();
-	}
-
-	public static class DummyRoutesBuild extends RouteBuilder {
-
-		@Override
-		public void configure() throws Exception {
-
-			from("timer:mytimer?period=2000").process(new Processor() {
-
-				public void process(Exchange exc) throws Exception {
-					System.out.println("we get called");
-					// TODO Auto-generated method stub
-					exc.getOut().setBody(
-							"Hello, Now it is " + new Date().toString());
-				}
-			}).to("log://com.androidyou.DummyRoutesBuilder");
-		}
-
 	}
 
 }
